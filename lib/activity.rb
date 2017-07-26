06b8607e44c99a2d769f37ada663658f94af8110
samuelssnider <sam_redins@yahoo.com>
2017-07-26 09:18:06 -0600

    third test

--- lib/activity.rb
+++ lib/activity.rb
@@ -1,2 +1,15 @@
 class Activity
+  attr_reader :name,
+              :participants
+
+  def initialize(name)
+    @name = name
+    @participants = {}
+  end
+
+  def add_participant(name, price)
+    hash = {name => price}
+    @participants.merge(hash)
+  end
+
 end
--- test/activity_test.rb
+++ test/activity_test.rb
@@ -2,6 +2,31 @@ require 'minitest'
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/activity'
+require 'pry'
 
 class ActivityTest < Minitest::Test
+
+  def test_it_can_be_initalized
+    ac = Activity.new("Lunch")
+  end
+
+  def test_it_has_a_name
+    ac = Activity.new("Bowling")
+    assert_equal "Bowling", ac.name
+  end
+
+  def test_it_starts_with_no_participants
+    ac = Activity.new("Bowling")
+    assert ac.participants.empty?
+  end
+
+  def test_participants_can_be_added
+    ac = Activity.new("Bowling")
+    assert ac.participants.empty?
+    ac.add_participant("Jim", 20)
+    binding.pry
+    assert_equal 1, ac.participants.count
+  end
+
+
 end
